780b3bc6a98c43b57c3ae7413a3647d90293f775
jiten kumar <jitendrakumar04@gmail.com>
2018-07-09 13:50:40 +0530

    Update lampstack.sh

--- lampstack.sh
+++ lampstack.sh
@@ -3,18 +3,21 @@
 sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get install -y apache2
 
-sed -i '$ a servername 35.176.109.10' /etc/apache2/apache2.conf
+sudo sed -i '$ a servername 35.178.91.221' /etc/apache2/apache2.conf
 sudo apache2ctl configtest
 sudo systemctl restart apache2
+sudo apt-get -y update
 
-echo mysql-server mysql-server/root_password select 9871204995 | debconf-set-selections
-echo mysql-server mysql-server/root_password_again select 9871204995 | debconf-set-selections
+sudo echo mysql-server mysql-server/root_password select 9871204995 | debconf-set-selections
+sudo echo mysql-server mysql-server/root_password_again select 9871204995 | debconf-set-selections
 sudo apt-get install -y mysql-server
 sudo systemctl start mysql
 
+sudo apt-get -y update
 apt-get install -y php libapache2-mod-php php-mcrypt php-mysql
 sudo chmod +x /etc/apache2/mods-enabled/dir.conf
 
+sudo apt-get -y update
 sudo cat > /etc/apache2/mods-enabled/dir.conf <<- "EOF"
 <IfModule mod_dir.c>
     DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
@@ -22,6 +25,8 @@ sudo cat > /etc/apache2/mods-enabled/dir.conf <<- "EOF"
 EOF
 sudo systemctl restart apache2
 sudo apt-get -y install php-cli
+
+sudo apt-get -y update
 sudo cat > /var/www/html/info.php <<- "EOF"
 <?php
 phpinfo();
